49454905fea2c79d7caae5f3750b59c4e632b8b2
Alisson <alisson.rosa@digifred.com.br>
2023-11-20 19:50:23 -0300

    back-01: update DB name + update sql infos

--- Back/db-sql/db-create.sql
+++ Back/db-sql/db-create.sql
@@ -1,23 +1,29 @@
+create database rottentomatoes;
+
+-- Se estiver criando o BD no servidor sem usuários, rodar:
+
+create user postgres with encrypted password 'postgres'
+grant all privileges on database rottentomatoes to postgres
+
 create table if not exists films (
 	i_film serial not null primary key,
-	title char(200) not null,
-	description char(500),
-    original_language char(200),
-    director char(200),
-	note float,
-	cover char(500),
+	title varchar(200) not null,
+	description varchar(500),
+    original_language varchar(200),
+    director varchar(200),
+	score float,
+	cover varchar(500),
 	duration int,
 	releaseYear smallint
 );
 
-
 create table if not exists reviews (
 	i_review serial not null primary key,
-	title char(200) not null,
-	analysis char(500) not null,
-	note float,
-	name char(200),
-	img char(500),
+	title varchar(200) not null,
+	analysis varchar(500) not null,
+	score float,
+	name varchar(200),
+	img varchar(500),
 	i_film int,
 	foreign key(i_film)
 		references films(i_film)
@@ -25,7 +31,7 @@ create table if not exists reviews (
 
 create table if not exists genres (
 	i_genre serial not null primary key,
-	name char(200)
+	name varchar(200)
 )
 
 create table if not exists genre_film (
@@ -36,3 +42,13 @@ create table if not exists genre_film (
 	foreign key(i_genre)
 		references genres(i_genre)
 )
+
+-- Versão das tabelas para rodar por meio do PSQL (shel do postgre)
+
+create table if not exists films (i_film serial not null primary key,title varchar(200) not null,description varchar(500),original_language varchar(200),director varchar(200),note float,cover varchar(500),duration int,releaseYear smallint);
+
+create table if not exists reviews (i_review serial not null primary key,title varchar(200) not null,analysis varchar(500) not null,score float,name varchar(200),img varchar(500),i_film int,foreign key(i_film) references films(i_film));
+
+create table if not exists genres (i_genre serial not null primary key,name varchar(200));
+
+create table if not exists genre_film (i_film int,i_genre int,foreign key(i_film) references films(i_film),foreign key(i_genre) references genres(i_genre));
--- Back/db-sql/db-values.sql
+++ Back/db-sql/db-values.sql
@@ -1,11 +1,11 @@
 -- Create films examples
 
-INSERT INTO films (i_film, title, description, genre, note, cover, duration, releaseyear) 
-	values (1, 'THE HUNGER GAMES: THE BALLAD OF SONGBIRDS & SNAKES', 'this is a film', 'Action, Adventure, Sci-fi', 67, 'caminha da img', 180, 2023);
+INSERT INTO films (title, description, score, cover, duration, releaseyear) 
+	values ('THE HUNGER GAMES: THE BALLAD OF SONGBIRDS & SNAKES', 'this is a film', 67, 'caminha da img', 180, 2023);
 
 --  create reviews examples
 
-INSERT INTO reviews (i_review, title, analysis, note, name, img, i_film) 
+INSERT INTO reviews (i_review, title, analysis, score, name, img, i_film) 
 	VALUES (1, 'this is definitely a movie', 'The Ballad of Songbirds & Snakes" takes us back to the dystopian world of Panem, exploring the origins of the Hunger Games and the rise of President Coriolanus Snow. Directed by Francis Lawrence, this prequel to the acclaimed Hunger Games series provides a fresh perspective on the Capitols dark history.', 4.5, 'jhonatas Junior', 'caminha da img do viado', 1);
 
 -- create genres examples
