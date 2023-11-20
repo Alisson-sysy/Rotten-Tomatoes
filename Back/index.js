ed60d6143fed2aab2f01c962ab5042b3c365ce23
Alisson <alisson.rosa@digifred.com.br>
2023-11-20 19:54:52 -0300

    back-01: add cors validation + update readme

--- Back/index.js
+++ Back/index.js
@@ -2,16 +2,31 @@ const express = require('express')
 const db = require('./db-conection')
 const app = express()
 let id;
+
+  //create the middleware
+  const corsValidation = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next(); // Calling this function invokes the next middleware function in the app 
+  }
+
+  app.use(corsValidation);
+
   // Primary path
   app.get('/', (request, response) => {
     response.send("Primary page")
   })
 
   // Get all films
-  app.get('/films', async (request, response) => {
+  app.get('/films/:pagina?', async (request, response) => {
     try {
-        const result = await db.query('SELECT * FROM films');
-        response.json(result.rows);
+      let params = 0;
+      if (request.params.pagina !== undefined) {
+        params = request.params.pagina;
+      }
+
+      const result = await db.query(`SELECT * FROM films LIMIT 5 offset ${params}`);
+
+      response.json(result.rows);
     } catch (error) {
         console.error(error);
         response.status(500).send('Internal Server Error'); 
@@ -28,6 +43,8 @@ let id;
     }
   });
 
+
+  // Genres
   app.get('/genre/:genre', async (request, response) => {
     try {
         const genre = await db.query(`SELECT i_genre FROM genres where name='${request.params.genre}'`);
@@ -49,8 +66,10 @@ let id;
       console.error(error);                                                     
       response.status(500).send('Internal Server Error'); 
     }
-  })
+  });
 
+
+  // Reviews
   app.get('/reviews', async (request, response) => {
     try {
         const result = await db.query('SELECT * FROM reviews');
@@ -63,7 +82,6 @@ let id;
 
   app.get('/films/:id', async (request, response) => {
     try {
-        console.log(`the id is: ${request.params.id}`);
         const result = await db.query(`SELECT * FROM films where i_film=${request.params.id}`);
         response.json(result.rows);
     } catch (error) {
@@ -75,4 +93,4 @@ let id;
   const PORT = 8000
   app.listen(PORT, () => {
     console.log(`Project run in port: ${PORT}`)
-  })
+  });
--- README.md
+++ README.md
@@ -18,3 +18,23 @@ O nome do BD deve ser 'rottenTomatoes';
 'npm run dev', para roda o projeto na versão com o nodemon (utilizado para identificar as modificações no código) ou npm 'npm run start'
 para rodar o projeto sem o nodemon;
 
+
+### Rodar o projeto no servidor
+
+
+#### Comandos úteis para o servidor
+
+Verificar se o postgres esta instalado no servidor: 'sudo systemctl status postgresql'
+
+Instalar postgressql via terminal: 'sudo apt install postgresql'
+
+Acessar o PSQL: sudo -u postgres psql
+
+Alterar senha usuario postgres: alter user <username> with encrypted password '<password>'
+
+listar tabelas no psql: \d
+listar usuarios no psql: \du
+listar banco de dados no psql: \l
+acessar um BD no psql: \c <nome_do_BD>
+sair do psql: '\q'
+
